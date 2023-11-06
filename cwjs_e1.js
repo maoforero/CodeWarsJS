33ec7b0f80016123c89bee5f9bc494f1ecf1a574
maoforero <maforeror@gmail.com>
2023-11-05 21:05:17 -0500

    exercise 1 complete

--- cwjs_e1.js
+++ cwjs_e1.js
@@ -1,3 +1,18 @@
-func hola()
-{console.log("Hello World!");}
+func getGrade(s1, s2, s3){
+  	let totalBase = 3;
+	let averageScore = (s1, s2, s3) / totalBase;
+	let score = null;
 
+	if(averageScore < 60){
+		return score = 'F';
+	}else if(averageScore < 70){
+		return score = 'D';
+	}else if(averageScore < 80{
+		return score = 'C';
+	}else if(averageScore < 90){
+		return score = 'B';
+	}else if(averageScore <= 100){
+		return score = 'A';
+	}
+	return score;
+}
